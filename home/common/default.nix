1d915d888227e7fcea48b7cb6fab42efec3f94ca
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2026-01-09 12:28:00 -0800

    comment files and move some things around

--- home/common/default.nix
+++ home/common/default.nix
@@ -8,16 +8,19 @@
 }:
 {
   imports = [
-    "${nhModules}/desktop/${configFile.desktop.environment}"
+    "${nhModules}/desktop/${configFile.desktop.environment}" # import nix config for desktop environment from config file
+    # default apps
     "${nhModules}/programs/gpg"
     "${nhModules}/programs/flatpak"
     "${nhModules}/programs/cli/fzf.nix"
     "${nhModules}/programs/cli/fastfetch.nix"
   ]
+  # import all desktop modules enabled in config file
   ++ map (
     name: "${nhModules}/desktop/${configFile.desktop.environment}/${name}"
   ) configFile.desktop.modules or [ ];
 
+  # i forgot why i added this, i think it was to update nix everytime i rebuilt home-manager because nix got outdated
   nix.package = pkgs.nix;
 
   nixpkgs = {
@@ -31,13 +34,21 @@
     };
   };
 
+  # required for home-manager to work
   systemd.user.startServices = "sd-switch";
+  programs.home-manager.enable = true;
+  home.stateVersion = "26.05";
 
+  # remove home manager news
+  news.display = "silent";
+
+  # sets up user config using the passed in name
   home = {
     username = "${userConfig.name}";
     homeDirectory = "/home/${userConfig.name}";
   };
 
+  # more default apps
   home.packages = with pkgs; [
     fd
     jq
@@ -45,8 +56,9 @@
     curl
     zip
     unzip
+    gcc
+    glib
 
-    openrazer-daemon
     libgbm
   ];
 }
--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -8,7 +8,7 @@
 {
   imports = [
     ./hardware-configuration.nix
-    "${nixosModules}/desktop/${configFile.desktop.environment}"
+    "${nixosModules}/desktop/${configFile.desktop.environment}" # import nix config to enable desktop environment from config file
     "${nixosModules}/programs/steam"
     # "${nixosModules}/services/disko/btrfs_with_windows.nix"
     inputs.nix-minecraft.nixosModules.minecraft-servers
@@ -21,21 +21,30 @@
     inputs.millennium.overlays.default
   ];
 
+  # use cachyos rc kernel
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-rc;
+
+  # enable open razer
+  hardware.openrazer.enable = true;
 
   networking.firewall = {
     allowedTCPPorts = [
+      # http
       80
+      # https
       443
+      # minecraft
       25565
     ];
     allowedTCPPortRanges = [
+      # kde connect
       {
         from = 1714;
         to = 1764;
       }
     ];
     allowedUDPPortRanges = [
+      # kde connect
       {
         from = 1714;
         to = 1764;
