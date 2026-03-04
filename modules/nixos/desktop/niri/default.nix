1d8fe6e6ed85626b4511b2f88032ada4e6a3150f
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2026-03-03 21:27:19 -0800

    patch niri to allow fullscreened transparent apps
    https://github.com/niri-wm/niri/discussions/1399
    
    patch from
    https://github.com/saygo-png/nixos/blob/d5223c8c16cc1efd9d229d9de05dc34f0c1616c3/resources/niri/transparent-fullscreen.patch
    
    a

--- modules/nixos/desktop/niri/default.nix
+++ modules/nixos/desktop/niri/default.nix
@@ -9,11 +9,20 @@
     inputs.niri.nixosModules.niri
   ];
 
-  nixpkgs.overlays = [ inputs.niri.overlays.niri ];
+  nixpkgs.overlays = [
+    inputs.niri.overlays.niri
+    (_: prev: {
+      niri-unstable-patched = prev.niri-unstable.overrideAttrs (old: {
+        patches = (old.patches or [ ]) ++ [
+          ./transparent-fullscreen.patch
+        ];
+      });
+    })
+  ];
 
   programs.niri = {
     enable = true;
-    package = pkgs.niri-unstable;
+    package = pkgs.niri-unstable-patched;
   };
 
   services.displayManager = {
